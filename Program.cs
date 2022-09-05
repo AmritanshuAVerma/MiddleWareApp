629fc594e465d342816f1e4fcb84d06c720f001f
AmritanshuAVerma <amritanshu.verma@outlook.com>
2022-09-05 16:39:58 +0530

    Created a Standard Middleware

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using MiddleWareApp.Controllers;
+using MiddleWareApp.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,6 +18,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseErrorHandlingMiddleware();
 
 app.UseHttpsRedirection();
 
